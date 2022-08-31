13b6476f917c23412249d7f32d89761781f63832
Ballyqueen <112537514+Ballyqueen@users.noreply.github.com>
2022-08-31 07:12:08 +0100

    Add files via upload

---
+++ Balikis Ass.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1 style="font-size:3rem;color:blue;">MY JUPYTER NOTEBOOK ON IBM WATSON STUDIO</h1>
+
+# # YEKEEN Balikis Oluwakemi
+#     I am 100% human, a new student and Future Data Scientist.
+
+# ##### I am interested in data science because i love dealing with fact and figures. Also it's an interesting course.
+
+# <h3> Mathematics Operation (+)</h3>
+
+# In[2]:
+
+
+A = 15
+B = 20
+A + B
+
+
+# ~~this is a mistake, im not allow to make.~~
+# - nife
+# - tobi
+# - ope
+# - dayo <br> the above name and the ID are shown below and their gender
+#  | NAMES  | ID NUMBER | GENDER|
+#  | -------| --------- | ------|
+#  | NIFE   | 1122330   | F     |
+#  | TOBI   | 1122331   | M     |
+#  | OPE    | 1122333   | F     |
+#  | DAYO   | 1122334   | M     |
+#  
+#     my plans, the ticked ones have been carried out.
+#     - [x] buy a house a house
+#     - [x] renoate the living room
+#     - [ ] apply for scholarship to study in Europe
+#     - [ ] go on vacation
+# 
